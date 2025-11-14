24cb837dd92dda6ed8b41296580b76b37a983361
cnrsbtogll <cnrsbtogll@gmail.com>
2025-11-14 14:06:17 +0300

    feat(theme): implement dynamic theming in App component
    - Integrate useThemeStore to manage dark mode state.
    - Update StatusBar style based on the current theme, enhancing user experience across the app.

--- App.tsx
+++ App.tsx
@@ -1,12 +1,15 @@
 import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { RootNavigator } from './src/navigation/RootNavigator';
+import { useThemeStore } from './src/store/useThemeStore';
 
 export default function App() {
+  const { isDarkMode } = useThemeStore();
+  
   return (
     <>
       <RootNavigator />
-      <StatusBar style="auto" />
+      <StatusBar style={isDarkMode ? 'light' : 'dark'} />
     </>
   );
 }
